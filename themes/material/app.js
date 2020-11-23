2a986d4e700d8f4711442873ff89fd0b578ef9cc
Shardul Pakhale <47375180+omssp@users.noreply.github.com>
2020-11-23 07:49:55 +0530

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -4,7 +4,7 @@ document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@0.4.3/d
 document.write('<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/plyr/3.6.2/plyr.css">');
 // markdown support
 document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script>');
-document.write('<style>.mdui-list-item:hover{color: #212121 !important; background-color: #FAFAFA !important;}.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>');
+document.write('<style>.mdui-list-item:hover{color: #212121; background-color: #FAFAFA;}.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>');
 if (dark) { document.write('<style>* {box-sizing: border-box}body{color:rgba(255,255,255,.87);background-color:#333232}.mdui-theme-primary-' + main_color + ' .mdui-color-theme{background-color:#232427!important}</style>'); }
 // Initialize the page and load the necessary resources
 function init() {
@@ -131,11 +131,11 @@ function list(path) {
         } else if (who == "sizeSort") {
             _files.sort((a, b) => (a.size - b.size))
         }
-        if (how == "arrow_downward") {
-            _files.reverse(); // descending
-            $("#" + who).html('arrow_upward');
-        } else {
+        if (how == "arrow_upward") {
             $("#" + who).html('arrow_downward');
+        } else {
+	    _files.reverse(); // descending
+            $("#" + who).html('arrow_upward');
         }
         list_files(_path, _files);
     });
